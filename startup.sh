13d0f40d3f18d5017f247816e01e5817e4d4a8af
Tobias Megies <megies@geophysik.uni-muenchen.de>
2019-10-24 13:12:59 +0200

    docker

--- startup.sh
+++ startup.sh
@@ -2,28 +2,28 @@
 #!/bin/bash
 
 # final steps are supposed to be done as notebook user
-echo `whoami` > $HOME/whoami
-echo `whoami` > /home/jovyan/whoami2
-if [ "$USER" != 'jovyan' ]; then exit 1; fi
-
-# update notebooks
-cd $HOME/seismo_live
-#git fetch origin
-#git reset --hard origin/master
-git pull
-
-# only expose notebooks in the jupyter home dir
-ls -l *
-cd $HOME
-rm -rf $HOME/work
-mv $HOME/seismo_live/notebooks/* $HOME/
-rm -rf $HOME/seismo_live
-ls -l *
-ls -l $HOME
-
-# XXX just for testing
-touch $HOME/testfile
-
-# XXX ugly hack to try and work around proj env issues
-# XXX https://github.com/conda-forge/basemap-feedstock/issues/30
-export PROJ_LIB=/opt/conda/share/proj/
+echo "$USER" > /tmp/whoami
+#echo `whoami` > /home/jovyan/whoami2
+#if [ "$USER" != 'jovyan' ]; then exit 1; fi
+#
+## update notebooks
+#cd $HOME/seismo_live
+##git fetch origin
+##git reset --hard origin/master
+#git pull
+#
+## only expose notebooks in the jupyter home dir
+#ls -l *
+#cd $HOME
+#rm -rf $HOME/work
+#mv $HOME/seismo_live/notebooks/* $HOME/
+#rm -rf $HOME/seismo_live
+#ls -l *
+#ls -l $HOME
+#
+## XXX just for testing
+#touch $HOME/testfile
+#
+## XXX ugly hack to try and work around proj env issues
+## XXX https://github.com/conda-forge/basemap-feedstock/issues/30
+#export PROJ_LIB=/opt/conda/share/proj/
